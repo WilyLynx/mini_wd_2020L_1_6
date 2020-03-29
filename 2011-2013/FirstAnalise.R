d1a25ff05a1b4bc433119606270437d82cb69c4f
Damian Kryński <krynskid@student.mini.pw.edu.pl>
2020-03-29 09:54:08 +0200

    update

--- 2011-2013/FirstAnalise.R
+++ 2011-2013/FirstAnalise.R
@@ -4,14 +4,21 @@ library(ggplot2)
 library(sqldf)
 
 # ====================================== FUNCTIONS =======================================
-second <- function(x) {
-  x[2]
+statistics <- function(x) {
+  a <- summarise(.data = x,
+                 grants_count = n(),
+                 budget_sum = sum(budget),
+                 budget_mean = mean(budget),
+                 monthly_budget_sum = sum(monthly_budget),
+                 monthly_budget_mean = mean(monthly_budget),
+                 monthly_budget_sd = sd(budget),
+                 monthly_budget_per_person_sum = sum(monthly_budget_per_person),
+                 monthly_budget_per_person_mean = mean(monthly_budget_per_person),
+                 monthly_budget_per_person_sd = sd(monthly_budget_per_person),
+                 person_count_mean = mean(coinvestigators),
+                 person_count_sd = sd(coinvestigators))
+  a
 }
-
-get_list <- function(x) {
-  x[[1]]
-}
-
 
 # ======================================== DATA ==========================================
 org_data <-
@@ -19,100 +26,146 @@ org_data <-
            stringsAsFactors = FALSE,
            encoding = "UTF-8")
 
+descriptors_data <- 
+  read.csv('../data/descriptors.csv',
+           stringsAsFactors = FALSE,
+           encoding = "UTF-8")
+
+org_data_not_fixed <-
+  read.csv('../data/grants_fixed.csv',
+           stringsAsFactors = FALSE,
+           encoding = "UTF-8",
+           sep = "#")
+
 all_contests_df <- mutate(org_data,
-                      year =               
-                        strsplit(start,split = "-") %>%    
-                        sapply(first) %>%    
-                        as.integer(),
-                      month =   
-                        strsplit(start,split = "-") %>%
-                        sapply(second) %>%
-                        as.integer(),
-                      day =
-                        strsplit(start,split = "-") %>%
-                        sapply(last) %>%
-                        as.integer(),
-                      monthly_budget = 
-                        budget/duration,
-                      monthly_budget_per_person = 
-                        monthly_budget/coinvestigators,
-                      descriptors_count = 
-                        strsplit(descriptors, split = "|", fixed=TRUE) %>% 
-                        sapply(unlist) %>% 
-                        sapply(length)
-                        
+                          year =
+                            strsplit(start, split = "-") %>%
+                            sapply(first) %>%
+                            as.integer(),
+                          month =
+                            strsplit(start, split = "-") %>%
+                            sapply(nth, n=2) %>%
+                            as.integer(),
+                          day =
+                            strsplit(start, split = "-") %>%
+                            sapply(last) %>%
+                            as.integer(),
+                          monthly_budget =
+                            budget / duration,
+                          monthly_budget_per_person =
+                            monthly_budget / coinvestigators,
+                          descriptors_count =
+                            strsplit(descriptors, split = "|", fixed = TRUE) %>%
+                            sapply(unlist) %>%
+                            sapply(length),
+                          subpanel =
+                            strsplit(descriptors, split = "|", fixed = TRUE) %>%
+                            sapply(first) %>%
+                            strsplit(split = "_", fixed = TRUE) %>%
+                            sapply(first),
+                          sorted_descriptors = 
+                            strsplit(descriptors, split = "|", fixed = TRUE) %>% 
+                            sapply(sort) %>%
+                            sapply(unlist) %>% 
+                            sapply(paste, collapse="|")
 )
 
+
+all_descriptors_df <- mutate(descriptors_data,
+                             year =
+                               strsplit(id, split = "/", fixed = TRUE) %>%
+                               sapply(first) %>%
+                               as.integer(),
+                             month =
+                               strsplit(id, split = "/", fixed = TRUE) %>%
+                               sapply(nth, n=2) %>%
+                               as.integer(),
+                             panel = 
+                               gsub(subpanel ,pattern = "[0-9]", replacement = "")
+                           )
+
 # =================================== VARIABLES ============================================
-years_range_sql = "select * from all_contests_df where year>=2011 and year <=2013 and coinvestigators IS NOT NULL"
-all_data_sql = "select * from all_contests_df"
+contest_df_years_range_sql = "select * from all_contests_df where year>=2011 and year <=2013 and coinvestigators IS NOT NULL"
+contest_df_specific_day = "select * from all_contests_df where year = 2011 and month = 12 and day = 1 and coinvestigators IS NOT NULL"
+contest_df_all_data_sql = "select * from all_contests_df"
+descriptors_df_years_range_sql = " select * from all_descriptors_df where year >=2011 and year <=2013"
+descriptors_df_all_data_sql = "select * from all_descriptors_df"
 
 
 
 # =================================== CONFIG ==============================================
-current_sql = years_range_sql
-contests_df <- sqldf(current_sql)
-
-contests_df[5693,]
+setwd("C:/Users/DAMIAN/Desktop/STUIDA/Magisterskie - In¿. i analiza danych/Semestr 1/Wizualizacja Danych/Repo/mini_wd_2020L_1_6/2011-2013")
+contests_current_sql = contest_df_years_range_sql
+descriptors_current_sql = descriptors_df_years_range_sql
+contests_df <- sqldf(contest_df_years_range_sql)
+descriptors_df <- sqldf(descriptors_current_sql)
 
 # ================================ ANALYSIS DATA============================================
 # --------------------- WITHOUT DIVISION TO PANELS OR CONTESTS -----------------------------
 #statystyki w poszczególnych latach 
 statistics_in_years <- 
-  contests_df %>% group_by(year) %>% summarise(grants_count = n(), 
-                                               budget_sum = sum(budget),
-                                               budget_mean = mean(budget),
-                                               monthly_budget_sum = sum(monthly_budget),
-                                               monthly_budget_mean = mean(monthly_budget),
-                                               monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                               monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(year) %>% statistics
 
 #statystyki w poszczególnych miesi¹cach (typ miesi¹c na przestrzeni lat)
 statistics_in_months <- 
-  contests_df %>% group_by(month) %>% summarise(grants_count = n(), 
-                                                budget_sum = sum(budget),
-                                                budget_mean = mean(budget),
-                                                monthly_budget_sum = sum(monthly_budget),
-                                                monthly_budget_mean = mean(monthly_budget),
-                                                monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                                monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(month) %>% statistics
 
 #statystyki w poszczególnych dniach (typ dzieñ na przestrzeni miesiêcy i lat)
 statistics_in_days <- 
-  contests_df %>% group_by(day) %>% summarise(grants_count = n(), 
-                                              budget_sum = sum(budget),
-                                              budget_mean = mean(budget),
-                                              monthly_budget_sum = sum(monthly_budget),
-                                              monthly_budget_mean = mean(monthly_budget),
-                                              monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                              monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(day) %>% statistics
 
 #statystyki w poszczególnych miesi¹cach w poszczególnych latach 
 statistics_in_years_and_months <- 
-  contests_df %>% group_by(year,month) %>%  summarise(grants_count = n(), 
-                                                      budget_sum = sum(budget),
-                                                      budget_mean = mean(budget),
-                                                      monthly_budget_sum = sum(monthly_budget),
-                                                      monthly_budget_mean = mean(monthly_budget),
-                                                      monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                                      monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(year,month) %>%  statistics
 
 #statystyki w poszczególnych dniach w poszczególnych miesi¹cach (na przestrzeni lat)
 statistics_in_months_and_days <- 
-  contests_df %>% group_by(month, day) %>%  summarise(grants_count = n(), 
-                                                      budget_sum = sum(budget),
-                                                      budget_mean = mean(budget),
-                                                      monthly_budget_sum = sum(monthly_budget),
-                                                      monthly_budget_mean = mean(monthly_budget),
-                                                      monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                                      monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(month, day) %>%  statistics
 
 #statystyki w poszczególnych dniach w poszczególnych miesi¹cach w poszczególnych latach
 statistics_in_years_months_and_days <- 
-  contests_df %>% group_by(year,month, day) %>%  summarise(grants_count = n(), 
-                                                           budget_sum = sum(budget),
-                                                           budget_mean = mean(budget),
-                                                           monthly_budget_sum = sum(monthly_budget),
-                                                           monthly_budget_mean = mean(monthly_budget),
-                                                           monthly_budget_per_person_sum = sum(monthly_budget_per_person),
-                                                           monthly_budget_per_person_mean = mean(monthly_budget_per_person))
+  contests_df %>% group_by(year,month, day) %>%  statistics
+
+#statystyki dla poszczególnych iloœci deskryptorów 
+statistics_descriptors_count <- 
+  contests_df %>% group_by(descriptors_count) %>%  statistics
+
+#statystyki dla poszczególnych konkursów
+statistics_in_contests <- 
+  contests_df %>% group_by(contest) %>% statistics
+
+#statystyki dla poszczególnych paneli 
+statistics_in_panels <-
+  contests_df %>%  group_by(panel) %>%  statistics
+
+#statystyki dla poszczególnych paneli w konkursach 
+statistics_in_contests_and_panels  <- 
+  contests_df %>%  group_by(contest, panel) %>%  statistics
+
+#statystyki dla poszczególnych subpaneli 
+statistics_in_subpanels <- 
+  contests_df %>%  group_by(subpanel) %>% statistics
+
+#iloœæ wniosków z podanymi deskryptorami 
+descriptors_popularity <- 
+  descriptors_df %>% group_by(descriptors) %>% summarise(count = n())
+
+#bud¿et w poszczególnych deksryptorach (i grupach deskryptorów)
+descriptors_budget <- 
+  contests_df %>% group_by(sorted_descriptors) %>%  statistics
+
+#statystyki wniosków w konkursach poszczególnych deksryptorów
+decriptors_in_contests <- 
+  contests_df %>% group_by(contest, sorted_descriptors) %>%  statistics
+
+#statystyki wniosków w panelach poszczególnych deksryptorów 
+descriptors_in_panels <-
+  contests_df %>% group_by(panel, sorted_descriptors) %>% statistics
+
+#statystyki wniosków po d³ugoœci trwania projektu 
+statistics_in_duration <- 
+  contests_df %>% group_by(duration) %>%  statistics
+
+#statystyki wniosków po iloœci osób 
+statistics_in_coinvestigators <- 
+  contests_df %>% group_by(coinvestigators) %>% statistics
